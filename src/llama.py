02b1a257297e3a544648df4aa00c0dd19856384c
david barkhuizen <david.barkhuizen@gmail.com>
2026-06-10 06:48:27 +0200

    minimum

--- src/llama.py
+++ src/llama.py
@@ -8,9 +8,26 @@ def new_client(url: str) -> AsyncClient:
 async def connect_and_start_session(host: str, port: int, model: str):
     client: AsyncClient = new_client(f"http://{host}:{port}")
 
-    message = {"content": "hello bot", "role": "user"}
-    stream = await client.chat(model=model, messages=[message], stream=True)
+    async def communicate(text: str) -> str:
 
-    async for part in stream:
-        content: str = part["message"]["content"]
-        print(content, end="", flush=True)
+        message = {"content": text, "role": "user"}
+
+        stream = await client.chat(model=model, messages=[message], stream=True)
+
+        reply: list[str] = []
+
+        async for part in stream:
+            content: str | list[str] = part["message"]["content"]
+
+            if isinstance(content, str):
+                reply.append(content)
+            if isinstance(content, list):
+                reply.extend([str(x) for x in content])
+
+            print(str(content), end="", flush=True)
+
+        return reply
+
+    while (utterance := input("> ")) != "exit":
+        response = await communicate(utterance)
+        print()
