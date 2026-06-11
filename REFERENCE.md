34190dc0ca23481540fbedd681aca11793e09ac8
david barkhuizen <david.barkhuizen@gmail.com>
2026-06-11 18:28:44 +0200

    sync

--- REFERENCE.md
+++ REFERENCE.md
@@ -2,7 +2,7 @@
 
 ## scan wifi network
 
-sudo arp-scan --interface=wlan0 --localnet
+    sudo arp-scan --interface=wlan0 --localnet
 
 ## ollama commands
 
@@ -16,9 +16,37 @@ sudo arp-scan --interface=wlan0 --localnet
 
     journalctl -u ollama -n 50 --no-pager
 
+### docker
 
-'
-docker exec -ti ollama ls -al /root/.ollama
+    docker exec -ti ollama ls -al /root/.ollama    
+    docker exec -ti ollama ollama list
+    docker exec -ti ollama ollama pull qwen3.6:35b
 
-docker exec -ti ollama ollama list
-docker exec -ti ollama ollama pull qwen3.6:35b
+## location of ollama files
+
+    /root/.ollama
+
+## .ssh file and folder privileges
+
+    .ssh folder           700         owner can read, write and exec, for permissions for group and others 
+    private key files     600         only owner can read and write
+    public key files      644         owner can read and write, group can read, others can read
+
+## alpine linux
+
+### configure ssh access for existing user
+
+from local machine, copy ssh-public-key to remote server, for existing user
+
+    ssh-copy-id -i ~/.ssh/solomon.id_ed25519.pub solomon@jebel
+
+### upgrade existing user to root
+
+    # install doas util
+    apk add doas
+
+    # add user solomon to wheel group
+    adduser solomon wheel
+
+    # Configure doas to permit wheel group members to run commands as root
+    echo 'permit :wheel as root' > /etc/doas.d/doas.conf
--- src/llama.py
+++ src/llama.py
@@ -8,26 +8,39 @@ def new_client(url: str) -> AsyncClient:
 async def connect_and_start_session(host: str, port: int, model: str):
     client: AsyncClient = new_client(f"http://{host}:{port}")
 
-    async def communicate(text: str) -> str:
+    async def invoke(invocation: str) -> str:
 
-        message = {"content": text, "role": "user"}
+        message = {"content": invocation, "role": "user"}
 
         stream = await client.chat(model=model, messages=[message], stream=True)
 
-        reply: list[str] = []
+        reply: str = ""
 
         async for part in stream:
             content: str | list[str] = part["message"]["content"]
 
-            if isinstance(content, str):
-                reply.append(content)
-            if isinstance(content, list):
-                reply.extend([str(x) for x in content])
-
-            print(str(content), end="", flush=True)
+            text: str = str(content)
+            reply += text
+            print(text, end="", flush=True)
 
         return reply
 
-    while (utterance := input("> ")) != "exit":
-        response = await communicate(utterance)
-        print()
+    async def execute_harness_command(command: str, args: list[str]) -> list[str]:
+        return list(["execute_harness_command"])
+
+    while (invocation := input("> ")) != "exit":
+        if invocation.startswith("!"):
+            command_text = invocation.strip()[1:]
+            command_response: list[str]
+            match command_text.split(" "):
+                case []:
+                    command_response = list(["empty harness command"])
+                case [command, *args]:
+                    command_response = await execute_harness_command(command, args)
+                case _:
+                    command_response = list()
+            for line in command_response:
+                print(line)
+        else:
+            _ = await invoke(invocation)
+            print()
