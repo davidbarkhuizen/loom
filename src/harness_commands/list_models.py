306089861a37bc7637a1fd94856287e30260fcb7
david barkhuizen <david.barkhuizen@gmail.com>
2026-06-13 20:34:30 +0200

    simplify handling in list-models

--- src/harness_commands/list_models.py
+++ src/harness_commands/list_models.py
@@ -2,21 +2,6 @@ from ollama._types import ListResponse, ModelDetails
 
 from common.markdown_utils import dicts_to_markdown_table, display_markdown
 from harness_commands.abstract import AbstractHarnessCommand
-from model import OllamaModel
-
-
-def ollama_model_from_list_models_response_model(model: ListResponse.Model) -> OllamaModel:
-
-    details: ModelDetails | None = model.details
-
-    return OllamaModel(
-        name=model.model if model.model else None,
-        size_MB=model.size // (1024 * 1024) if model.size else 0,
-        family=details.family if details else None,
-        format=details.format if details else None,
-        parameters=details.parameter_size if details and details.parameter_size else None,
-        quantization=details.quantization_level if details else None,
-    )
 
 
 class ListModelsCommand(AbstractHarnessCommand):
@@ -27,8 +12,18 @@ class ListModelsCommand(AbstractHarnessCommand):
     async def execute(self, args: list[str]) -> None:
         list_response: ListResponse = await self.client().list()
 
-        models: list[OllamaModel] = [
-            ollama_model_from_list_models_response_model(list_model) for list_model in list_response.models
+        FIELDS_TO_EXCLUDE = ["modified_at", "parent_model", "families"]
+
+        model_dicts = [
+            {k: v for k, v in model.__dict__.items() if k not in FIELDS_TO_EXCLUDE} for model in list_response.models
         ]
 
-        display_markdown(dicts_to_markdown_table([model.__dict__ for model in models]))
+        for model_dict in model_dicts:
+            model_dict["digest"] = model_dict["digest"][:8] + "..." + model_dict["digest"][-8:]
+            details_object = model_dict.pop("details")
+            details = {k: v for k, v in details_object.__dict__.items() if k not in FIELDS_TO_EXCLUDE}
+            model_dict.update(details)
+
+        model_dicts = sorted(model_dicts, key=lambda d: d["model"])
+
+        display_markdown(dicts_to_markdown_table(model_dicts))
